62a1419f8efa0b3f57404694f7b98a8e94a3202b
han-bbo <78396716+han-bbo@users.noreply.github.com>
2022-05-17 17:47:33 +0900

    エフェクト以外完成

--- www/src/emitted/index.js
+++ www/src/emitted/index.js
@@ -25,12 +25,10 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const PIXI = __importStar(require("pixi.js"));
 const preload_js_1 = require("./preload.js");
-const scor_point = 0; //スコアポイント
 const sprite_size = 200; //スプライトの大きさ
 const horizon = 200; //水平位置
-const play_time = 700000; //プレイ時間(ミリ秒数)
+const play_time = 100000; //プレイ時間(ミリ秒数)
 const move_dx = 5; //プレーむごと移動幅
-const interval_time = 10000; //敵キャラの出現間隔
 const player_animated_speed = 0.1; //プレイヤのアニメスピード
 const scaleX = window.innerWidth / 1280;
 const scaleY = window.innerHeight / 720;
@@ -45,14 +43,17 @@ let plyer_base_texture;
 let player_animated_data = {}; //プレイヤのanimatedスプライト配列
 let enemy_animated_data = {}; //プレイヤのanimatedスプライト配列
 let player; //プレイヤのスプライト
-let effect = null; //効果のスプライト
-let score = null; //スコアラベル
-let bg_image = null; //背景イメージ
+let effect; //効果のスプライト
+let score; //スコアラベル
+let bg_image; //背景イメージ
 let enemy_data = []; //敵キャラの保管用配列
 let item_data = []; //アイテムの保管用配列
 let move_point = 0; //プログラムが使用
 let touch_flg = false; //自キャラをタッチしたか否か
 let touch_dx = 0; //自キャラをタッチした時のジャンプ幅
+let interval_time = 10000; //敵キャラの出現間隔
+let score_point = 0; //スコアポイント
+let enemy_img = []; //敵のイメージ値
 window.onload = () => {
     var _a;
     app = new PIXI.Application({
@@ -60,6 +61,9 @@ window.onload = () => {
         height: 720 * scaleY
     });
     new preload_js_1.preload(app);
+    enemy_img.push(app.loader.resources.enemyA.url);
+    enemy_img.push(app.loader.resources.enemyB.url);
+    enemy_img.push(app.loader.resources.enemyC.url);
     //背景作成
     bg_image = PIXI.Sprite.from(app.loader.resources.background.url);
     bg_image.anchor.set(0);
@@ -88,18 +92,19 @@ window.onload = () => {
     effect.visible = false;
     app.stage.addChild(effect);
     // スコア作成
-    score = new PIXI.Text("SCORE: 0", {
+    score = new PIXI.Text("SCORE：0", {
         fill: "#ff0000",
         fontSize: 48,
         fontWeight: "bold",
         fontFamily: 'Times',
     });
-    score.width = 500 * scaleX;
+    score.width = 250 * scaleX;
     score.x = 50 * scaleX;
     score.y = 0;
     app.stage.addChild(score);
     move_point = player.x;
     app.stop();
+    enemy_data = new Array();
     (_a = document.getElementById("msg")) === null || _a === void 0 ? void 0 : _a.addEventListener("pointerdown", () => {
         app.start();
         document.body.removeChild(document.getElementById("msg"));
@@ -121,14 +126,14 @@ let create_player_texture = () => {
     player = new PIXI.AnimatedSprite(player_animated_data.stop);
 };
 let create_enemy_texture = () => {
-    let enemy_base_texture = PIXI.BaseTexture.from(app.loader.resources.player.url);
+    let img_num = Math.floor(Math.random() * 3);
+    let enemy_base_texture = PIXI.BaseTexture.from(enemy_img[img_num]);
     Object.entries(walk).forEach(([index, object]) => {
-        player_animated_data[index] = new Array();
+        enemy_animated_data[index] = new Array();
         object.forEach(element => {
-            player_animated_data[index].push(new PIXI.Texture(plyer_base_texture, new PIXI.Rectangle(element * sprite_size, 0, sprite_size, sprite_size)));
+            enemy_animated_data[index].push(new PIXI.Texture(enemy_base_texture, new PIXI.Rectangle(element * sprite_size, 0, sprite_size, sprite_size)));
         });
     });
-    player = new PIXI.AnimatedSprite(player_animated_data.stop);
 };
 let gameloop = (delta) => {
     move_sprite();
@@ -168,6 +173,14 @@ let move_sprite = () => {
     if (player.x - move_point >= move_dx) {
         player.x -= move_dx;
     }
+    enemy_data.forEach((object) => {
+        if (object.x < player.x - sprite_size / 2) {
+            object.x += move_dx / 2;
+        }
+        if (object.x > player.x + sprite_size / 2) {
+            object.x -= move_dx / 2;
+        }
+    });
     if (touch_flg) {
         player.y += touch_dx;
         touch_dx += 2;
@@ -182,12 +195,84 @@ let move_sprite = () => {
     }
 };
 let checkEnemy = () => {
+    enemy_data.forEach((object, index) => {
+        if (get_collision(object, player)) {
+            if (touch_dx > 0) {
+                effect.x = object.x;
+                effect.y = object.y;
+                effect.visible = true;
+                update_score(100);
+            }
+            else {
+                update_score(-10);
+            }
+            app.stage.removeChild(object);
+            enemy_data.splice(index, 1);
+        }
+    });
+};
+let get_collision = (object_1, object_2) => {
+    const bounds1 = object_1.getBounds();
+    const bounds2 = object_2.getBounds();
+    return bounds1.x < bounds2.x + bounds2.width
+        && bounds1.x + bounds1.width > bounds2.x
+        && bounds1.y < bounds2.y + bounds2.height
+        && bounds1.y + bounds1.height > bounds2.y;
 };
 let checkItem = () => {
+    item_data.forEach((object, index) => {
+        if (get_collision(object, player)) {
+            effect.x = object.x;
+            effect.y = object.y;
+            effect.visible = true;
+            app.stage.removeChild(object);
+            item_data.splice(index, 1);
+            update_score(300);
+        }
+    });
 };
 let makeEnemy = () => {
+    create_enemy_texture();
+    let enemy;
+    let x = Math.floor(Math.random() * app.stage.width - sprite_size);
+    enemy = player.x > x ? new PIXI.AnimatedSprite(enemy_animated_data.right) : new PIXI.AnimatedSprite(enemy_animated_data.left);
+    enemy.x = x;
+    enemy.y = horizon;
+    enemy.animationSpeed = 0.1;
+    enemy.play();
+    enemy_data.push(enemy);
+    app.stage.addChild(enemy);
+    if (game_flg) {
+        interval_time -= interval_time / 10;
+        setTimeout(makeEnemy, interval_time);
+    }
 };
 let makeItem = () => {
+    let pitem;
+    pitem = PIXI.Sprite.from(app.loader.resources.item.url);
+    pitem.scale.x *= 0.75;
+    pitem.scale.y *= 0.75;
+    pitem.x = Math.floor(Math.random() * (app.stage.width - sprite_size));
+    pitem.y = horizon - Math.floor(Math.random() * sprite_size);
+    app.stage.addChild(pitem);
+    item_data.push(pitem);
+};
+let update_score = (n) => {
+    score_point += n;
+    score_point = score_point < 0 ? 0 : score_point;
+    score.text = "SCORE：" + score_point;
 };
 let gameEnd = () => {
+    game_flg = false;
+    let msg = new PIXI.Text("GAME OVER", {
+        fill: "red",
+        fontFamily: "Times",
+        fontSize: 96,
+        fontWeight: "bold"
+    });
+    msg.x = 50;
+    msg.y = 50;
+    msg.width = 800;
+    app.stage.addChild(msg);
+    //app.stop();
 };
--- www/src/index.ts
+++ www/src/index.ts
@@ -1,12 +1,11 @@
 import * as PIXI from "pixi.js"
+import { Texture } from "pixi.js";
 import {preload} from "./preload.js"
 
-const scor_point = 0;//スコアポイント
 const sprite_size = 200; //スプライトの大きさ
 const horizon = 200; //水平位置
-const play_time = 700000; //プレイ時間(ミリ秒数)
+const play_time = 100000; //プレイ時間(ミリ秒数)
 const move_dx = 5; //プレーむごと移動幅
-const interval_time = 10000; //敵キャラの出現間隔
 const player_animated_speed = 0.1;//プレイヤのアニメスピード
 const scaleX = window.innerWidth/1280;
 const scaleY = window.innerHeight/720;
@@ -28,17 +27,18 @@ let enemy_animated_data:{
 } = {}//プレイヤのanimatedスプライト配列
 
 let player:PIXI.AnimatedSprite; //プレイヤのスプライト
-let effect = null; //効果のスプライト
-let score = null; //スコアラベル
-let bg_image = null//背景イメージ
-let enemy_data = []; //敵キャラの保管用配列
-let item_data = []; //アイテムの保管用配列
+let effect:PIXI.Sprite; //効果のスプライト
+let score:PIXI.Text; //スコアラベル
+let bg_image:PIXI.Sprite;//背景イメージ
+let enemy_data:Array<PIXI.AnimatedSprite> = []; //敵キャラの保管用配列
+let item_data:Array<PIXI.Sprite> = []; //アイテムの保管用配列
 let move_point = 0; //プログラムが使用
-
 let touch_flg = false;//自キャラをタッチしたか否か
 let touch_dx = 0;//自キャラをタッチした時のジャンプ幅
+let interval_time = 10000; //敵キャラの出現間隔
+let score_point:number = 0;//スコアポイント
 
-
+let enemy_img:Array<string> = []//敵のイメージ値
 
 window.onload = () => {
     
@@ -48,6 +48,10 @@ window.onload = () => {
     })
 
     new preload(app);
+
+    enemy_img.push(app.loader.resources.enemyA.url);
+    enemy_img.push(app.loader.resources.enemyB.url);
+    enemy_img.push(app.loader.resources.enemyC.url);
     
     //背景作成
     bg_image = PIXI.Sprite.from(app.loader.resources.background.url);
@@ -80,13 +84,13 @@ window.onload = () => {
     app.stage.addChild(effect)
 
     // スコア作成
-    score = new PIXI.Text("SCORE: 0", {
+    score = new PIXI.Text("SCORE：0", {
         fill: "#ff0000",
         fontSize: 48,
         fontWeight: "bold",
         fontFamily: 'Times',
     })
-    score.width = 500 * scaleX;
+    score.width = 250 * scaleX;
     score.x = 50*scaleX;
     score.y = 0;
     app.stage.addChild(score)
@@ -94,6 +98,8 @@ window.onload = () => {
     move_point = player.x;
 
     app.stop();
+
+    enemy_data = new Array();
 
     document.getElementById("msg")?.addEventListener("pointerdown", () => {
         
@@ -123,17 +129,16 @@ let create_player_texture = () => {
 }
 
 let create_enemy_texture = () => {
-    let enemy_base_texture = PIXI.BaseTexture.from(app.loader.resources.player.url);
+    let img_num = Math.floor(Math.random() * 3);
+    let enemy_base_texture = PIXI.BaseTexture.from(enemy_img[img_num]);
 
     Object.entries(walk).forEach(([index, object])  => {
-        player_animated_data[index] = new Array();
+        enemy_animated_data[index] = new Array();
 
         object.forEach(element => {
-            player_animated_data[index].push(new PIXI.Texture(plyer_base_texture, new PIXI.Rectangle(element*sprite_size,0,sprite_size,sprite_size)))
+            enemy_animated_data[index].push(new PIXI.Texture(enemy_base_texture, new PIXI.Rectangle(element*sprite_size,0,sprite_size,sprite_size)))
         })
     })
-    
-    player = new PIXI.AnimatedSprite(player_animated_data.stop);
 }
 
 let gameloop =(delta:any) => {
@@ -185,6 +190,15 @@ let move_sprite = () => {
         player.x -= move_dx;
     }
 
+    enemy_data.forEach((object) => {
+        if(object.x < player.x - sprite_size/2){
+            object.x += move_dx/2;
+        } 
+        if(object.x > player.x + sprite_size/2) {
+            object.x -= move_dx/2;
+        }
+    })
+
     if(touch_flg) {
         player.y += touch_dx;
         touch_dx += 2;
@@ -200,19 +214,95 @@ let move_sprite = () => {
 }
 
 let checkEnemy = () => {
+    enemy_data.forEach((object,index) => {
+        if(get_collision(object, player)) {
+            if(touch_dx > 0) {
+                effect.x = object.x;
+                effect.y = object.y;
+                effect.visible = true;
+                update_score(100)
+            } else {
+                update_score(-10)
+            }
+            app.stage.removeChild(object)
+            enemy_data.splice(index,1)
+        }
+    })
+}
 
+let get_collision = (object_1:PIXI.Sprite, object_2:PIXI.Sprite) => {
+    const bounds1 = object_1.getBounds();
+    const bounds2 = object_2.getBounds();
+
+    return bounds1.x < bounds2.x + bounds2.width
+        && bounds1.x + bounds1.width > bounds2.x
+        && bounds1.y < bounds2.y + bounds2.height
+        && bounds1.y + bounds1.height > bounds2.y;
 }
 
 let checkItem = () => {
+    item_data.forEach((object,index) => {
+        if(get_collision(object,player)) {
+            effect.x = object.x;
+            effect.y = object.y;
+            effect.visible = true;
 
+            app.stage.removeChild(object)
+            item_data.splice(index,1);
+            update_score(300)
+        }
+    })
 }
 
 let makeEnemy =() => {
+    create_enemy_texture();
+    let enemy:PIXI.AnimatedSprite;
+    let x = Math.floor(Math.random() * app.stage.width - sprite_size);
+    enemy = player.x>x ? new PIXI.AnimatedSprite(enemy_animated_data.right):new PIXI.AnimatedSprite(enemy_animated_data.left);
+    enemy.x = x;
+    enemy.y = horizon;
+    enemy.animationSpeed = 0.1;
+    enemy.play();
+
+    enemy_data.push(enemy);
+    app.stage.addChild(enemy);
+    if(game_flg) {
+        interval_time -= interval_time/10;
+        setTimeout(makeEnemy,interval_time);
+    }
 }
 
 let makeItem = () => {
+    let pitem:PIXI.Sprite
+    pitem = PIXI.Sprite.from(app.loader.resources.item.url);
+    pitem.scale.x *= 0.75;
+    pitem.scale.y *= 0.75;
+    pitem.x = Math.floor(Math.random() * (app.stage.width - sprite_size));
+    pitem.y = horizon - Math.floor(Math.random() *  sprite_size);
 
+    app.stage.addChild(pitem)
+    item_data.push(pitem)
+
+} 
+
+let update_score = (n:number) => {
+    score_point += n
+    score_point = score_point <0 ? 0 : score_point;
+    score.text = "SCORE：" +score_point;
 }
 
 let gameEnd = () => {
+    game_flg = false;
+    let msg = new PIXI.Text("GAME OVER", {
+        fill: "red",
+        fontFamily:"Times",
+        fontSize:96,
+        fontWeight: "bold"
+    })
+    msg.x = 50;
+    msg.y = 50;
+    msg.width = 800;
+    app.stage.addChild(msg)
+    
+    //app.stop();
 }
